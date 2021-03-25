d0efe841985382c3e46fb7d151a99e357b5cc7aa
rohitkuk <rohit.kukreja01@gmail.com>
2021-03-25 21:58:39 +0530

    ToDos Defined

--- __main__.py
+++ __main__.py
@@ -1,11 +1,37 @@
+
 from Data import explore, process
-# explore.show_grid(GRIDSIZE=1, ROWS=1, COLS=1)
 from torch.utils.data import DataLoader, Dataset
 from torchvision.utils import save_image
 from common.utils import create_dir, check_dir_exists
 
 
+"""
+  ToDos:
+    - Data Prepare              : Done
+    - Data Explore              : Done
+    - Data Process              : Done
+    - Data Augmentations        : Done
+    - Generator                 : WIP
+    - Disriminator              : TBD
+    - Training                  : TBD
+    - TensorBoard Integration   : TBD
+    - WandB Integration         : TBD
+    - Logging                   : TBD (Try Implementing Rich Library)
+    - Argument Parsing          : TBD
+    - Requirments.txt           : TBD
+    - Packaging                 : TBD
+    - Frontend or UI            : TBD
+    - Optimization              : Continous
+    - Test
+        - Unit                  : TBD
+        - Integration           : TBD
+        - System                : TBD
+        - UAT                   : TBD
+"""
+
+
 if __name__ == "__main__":
+    explore.show_grid(GRIDSIZE=1, ROWS=1, COLS=1)
     dataset = process.Pix2PixDatasets(dataset_name="edges2shoes")
     loader = DataLoader(dataset, batch_size=5)
     for x, y in loader:
